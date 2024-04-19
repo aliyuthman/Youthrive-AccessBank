6ba2280d01c58c311b04a6d682f325512606b906
Aliyu Usman <aliyuusman64@gmail.com>
2024-04-19 11:40:05 +0100

    Assignment 2 - Categorize Student Scores

---
+++ 1-1-SecondAssignment/Task.md
@@ -0,0 +1,6 @@
+ Write a JavaScript function named scoreCategory that takes a student's score as input and returns the corresponding category based on the following criteria:
+
+Score 90 or above: return "Excellent"
+Score 80-89: return "Very Good"
+Score 70-79: return "Pass"
+Score below 70: return "Fail".
---
+++ 1-1-SecondAssignment/scoreCategory.js
@@ -0,0 +1,29 @@
+const scoreCategory = (studentScore) => {
+  // Storing student categories as object of "category"
+  const category = {
+    excellent: studentScore >= 90,
+    veryGood: studentScore >= 80 && studentScore <= 89,
+    pass: studentScore >= 70 && studentScore <= 79,
+    fail: studentScore < 70,
+  };
+
+  // checking each category
+
+  if (category.excellent) {
+    return "Excellent";
+  } else if (category.veryGood) {
+    return "Very Good";
+  } else if (category.pass) {
+    return "Pass";
+  } else if (category.fail) {
+    return "Fail";
+  } else {
+    return "Wrong input, kindly check again!";
+  }
+};
+
+//saving the return results of the scoreCategory function in memory
+const studentCategory = scoreCategory(50);
+
+// printing to console
+console.log(studentCategory);
